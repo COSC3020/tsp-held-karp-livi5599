7e04d64e745d33651092ce20043312e5f2202401
livi5599 <obrown1@uwyo.edu>
2024-12-10 23:50:27 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,38 @@
 function tsp_hk(distance_matrix) {
-    return -1;
+    const cities = new Set();
+    for (let city = 0; city < distance_matrix.length; city++) {
+        cities.add(city);
+    }
+    //console.log(cities);
+    let start = [...cities][0];
+
+    console.log(distance_matrix[start]);
+    
+    heldKarp(distance_matrix, cities, start);
+    //return -1;
+}
+
+function heldKarp(distance_matrix, cities, start) {
+    const cache = new Map();
+    let tourLen = undefined;
+    for (city of cities) {
+        if (distance_matrix[start][city] != 0 && city != start) {
+            var end = city;
+            break;
+        }
+    }
+    //console.log("end = ", end);
+    if (Math.abs(cities.size) == 2) {
+        tourLen = distance_matrix[start][end];
+    }
+    //console.log("smallTourLen = ", smallTourLen);
+    else {
+        //return the min of
+        for (city of cities) {
+            if (city == start) {
+                continue;
+            }
+            
+        }
+    }
 }
